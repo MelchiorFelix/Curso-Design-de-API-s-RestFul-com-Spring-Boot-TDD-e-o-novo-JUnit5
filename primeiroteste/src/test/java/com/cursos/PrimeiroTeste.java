dfa276d4b89725a957c71e2b3fd91ed2a9b539fe
melchiorfelix <melchior.alencar@gmail.com>
2020-05-16 20:41:58 -0500

    Assertivas com AssertJ

---
+++ primeiroteste/src/test/java/com/cursos/PrimeiroTeste.java
@@ -0,0 +1,20 @@
+package com.cursos;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class PrimeiroTeste {
+
+    @Test
+    public void estruturaDeUmTeste(){
+            // cenário
+            int numero1 = 10, numero2 = 5;
+
+            //execução
+            int resultado = numero1 + numero2;
+
+            //verificações
+            Assertions.assertThat(resultado).isPositive();
+    }
+}
