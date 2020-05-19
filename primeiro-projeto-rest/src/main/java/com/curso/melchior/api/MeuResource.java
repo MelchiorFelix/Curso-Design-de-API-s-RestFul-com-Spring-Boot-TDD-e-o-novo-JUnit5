a4d6dac057cc61605f65e1c4625923a3e9b27748
melchiorfelix <melchior.alencar@gmail.com>
2020-05-18 20:17:13 -0500

    Básico para criação de uma API Rest na prática com Spring boot parte 2

--- primeiro-projeto-rest/src/main/java/com/curso/melchior/api/MeuResource.java
+++ primeiro-projeto-rest/src/main/java/com/curso/melchior/api/MeuResource.java
@@ -1,15 +1,34 @@
 package com.curso.melchior.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/cliente/")
+@RequestMapping("/api/cliente")
 public class MeuResource {
 
-    @GetMapping("{id}")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody Cliente cliente){
+        //service.save(cliente);
+        return cliente;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        //service.buscarPorId(id);
+        //service.delete(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public Cliente atualizar(@PathVariable Long id, @RequestBody Cliente cliente){
+        //service.buscarPorId(id);
+        //service.update(cliente);
+        return cliente;
+    }
+
+    @GetMapping("/{id}")
     public Cliente obertDadosCliente(@PathVariable Long id){
         System.out.println(String.format("Id recebido via url: %d", id));
         return new Cliente("John", "000.000.000-00");
