0cb9fd2b7aaa7657a211ce936aa61245475823ec
melchiorfelix <melchior.alencar@gmail.com>
2020-05-19 04:10:44 -0500

    Refatorando o teste para receber json via requisição

--- library-api/src/main/java/com/melchior/cursos/libraryapi/api/dto/BookDTO.java
+++ library-api/src/main/java/com/melchior/cursos/libraryapi/api/dto/BookDTO.java
@@ -1,10 +1,12 @@
 package com.melchior.cursos.libraryapi.api.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookDTO {
 
     private Long id;
--- library-api/src/main/java/com/melchior/cursos/libraryapi/api/resource/BookController.java
+++ library-api/src/main/java/com/melchior/cursos/libraryapi/api/resource/BookController.java
@@ -2,10 +2,7 @@ package com.melchior.cursos.libraryapi.api.resource;
 
 import com.melchior.cursos.libraryapi.api.dto.BookDTO;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/books")
@@ -13,12 +10,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDTO create(){
-        BookDTO dto = new BookDTO();
-        dto.setTitle("Meu Livro");
-        dto.setAuthor("Autor");
-        dto.setIsbn("1213212");
-        dto.setId((long) 11);
+    public BookDTO create(@RequestBody BookDTO dto){
         return  dto;
     }
 }
