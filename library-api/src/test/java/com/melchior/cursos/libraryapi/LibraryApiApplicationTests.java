a9e60690655ec06f136b943f4d2c0e89d4a687f3
melchiorfelix <melchior.alencar@gmail.com>
2020-05-18 23:29:56 -0500

    Criação e importação do projeto na IDE

---
+++ library-api/src/test/java/com/melchior/cursos/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melchior.cursos.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
