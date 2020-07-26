5afdbcb5b9ca8fa0b00dc32dbfb51ca847e46d3b
Melchior Felix <melchior.alencar@gmail.com>
2020-07-26 19:07:21 -0300

    Finalizando os testes de integração de empréstimos

--- library-api/src/test/java/com/melchiorfelix/libraryapi/model/repository/LoanRepositoryTest.java
+++ library-api/src/test/java/com/melchiorfelix/libraryapi/model/repository/LoanRepositoryTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -31,19 +33,39 @@ public class LoanRepositoryTest {
     @DisplayName("Deve verificar se existe empréstimo não devolvido para o livro.")
     public void existsByBookAndNotReturned(){
         //cenario
-        Book book = newBook();
-        entityManager.persist(book);
-        Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
-
-        entityManager.persist(loan);
+        Loan loan = createAndPersistLoan();
 
         //execucao
-        boolean exists = repository.existsByBookAndNotReturned(book);
+        boolean exists = repository.existsByBookAndNotReturned(loan.getBook());
 
         //verificacao
         assertThat(exists).isTrue();
 
     }
 
+    @Test
+    @DisplayName("Deve buscar empréstimo pelo isbn do livro ou customer")
+    public void findByBookIsbnOrCustomer(){
+        //cenario
+        Loan loan = createAndPersistLoan();
 
+        //execuccao
+        Page<Loan> result = repository.findByBookIsbnOrCustomer("123", "Fulano", PageRequest.of(0, 10));
+
+        //verificacao
+        assertThat(result.getContent()).hasSize(1);
+        assertThat(result.getContent()).contains(loan);
+        assertThat(result.getPageable().getPageSize()).isEqualTo(10);
+        assertThat(result.getPageable().getPageNumber()).isEqualTo(0);
+        assertThat(result.getTotalElements()).isEqualTo(1);
+    }
+
+    public Loan createAndPersistLoan(){
+        Book book = newBook();
+        entityManager.persist(book);
+        Loan loan = Loan.builder().book(book).customer("Fulano").loanDate(LocalDate.now()).build();
+        entityManager.persist(loan);
+
+        return loan;
+    }
 }
